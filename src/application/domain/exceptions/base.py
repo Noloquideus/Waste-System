91ca94e0baf9a8470e198926bbd7a77046a8c496
Noloquideus <daniilmanukian@gmail.com>
2024-10-18 14:01:11 +0300

    feat: add base business logic exception

---
+++ src/application/domain/exceptions/base.py
@@ -0,0 +1,20 @@
+from src.core.enums.status_code import StatusCode
+from src.core.objects.immutable import Immutable
+from src.core.objects.incomparable import Incomparable
+
+
+class ApplicationException(Exception, Incomparable, Immutable):
+    def __init__(self, status_code: StatusCode, message: str):
+        self._status_code = status_code
+        self._message = message
+
+    @property
+    def status_code(self) -> StatusCode:
+        return self._status_code
+
+    @property
+    def message(self) -> str:
+        return self._message
+
+    def __str__(self) -> str:
+        return f'{self.status_code} - {self.message}'
