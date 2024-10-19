8822d3c9e323960bb0794131c6240ca871290a5c
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:38:03 +0300

    feat: add organization entity

---
+++ src/application/domain/entities/organization.py
@@ -0,0 +1,28 @@
+import uuid
+
+from src.application.domain.entities.base import Entity
+from src.application.domain.value_objects.id import ID
+from src.application.domain.value_objects.name import Name
+from src.core.decorators.validate_types import validate_types
+from src.core.objects.sealed import Sealed
+
+
+class OrganizationEntity(Sealed, Entity):
+
+    @validate_types
+    def __init__(
+            self,
+            id: ID = None,
+            name: Name = None,
+    ):
+        self._id = id
+        self._name = name
+        super().__init__()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
