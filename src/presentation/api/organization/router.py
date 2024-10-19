e64169e0206ab123d7928df65abe0ca1019881c2
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:56:14 +0300

    feat: create get_all endpoint

--- src/presentation/api/organization/router.py
+++ src/presentation/api/organization/router.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from fastapi import APIRouter
 from fastapi.params import Query, Path, Depends
 from pydantic import StrictStr, StrictInt
@@ -8,14 +10,16 @@ from src.container import Container
 organization_router = APIRouter(prefix='/organization', tags=['Organization'])
 
 @organization_router.post(path='/', status_code=201)
-async def create_organization(name: StrictStr = Query(title='Name of organization')):
+async def create_organization(name: StrictStr = Query(title='Name of organization')) -> Dict[str, str]:
     service: IOrganizationService = await Container.get_service(IOrganizationService)
     organization: OrganizationEntity = await service.create(name)
     return organization.to_dict()
 
 @organization_router.get(path='/', status_code=200)
-async def get_organizations():
-    pass
+async def get_organizations() -> List[Dict[str, str]]:
+    service: IOrganizationService = await Container.get_service(IOrganizationService)
+    organizations: List[OrganizationEntity] = await service.get_all()
+    return [organization.to_dict() for organization in organizations]
 
 @organization_router.get(path='/{organization_id}', status_code=200)
 async def get_organization(organization_id: StrictInt = Path(title='ID of organization')):
