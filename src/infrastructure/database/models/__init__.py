e4704ca3e90eee615c880ae12c6b5693e638cf6f
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:37:01 +0300

    feat: add init file for models

---
+++ src/infrastructure/database/models/__init__.py
@@ -0,0 +1,5 @@
+from src.infrastructure.database.models.base import Base
+from src.infrastructure.database.models.organization import Organization
+from src.infrastructure.database.models.storage import Storage
+from src.infrastructure.database.models.waste_types import WasteType
+from src.infrastructure.database.models.waste_transfers import WasteTransfer
