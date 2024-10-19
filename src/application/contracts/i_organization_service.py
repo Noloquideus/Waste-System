b479415ca81c5665e0caef7616334f7f1b944c4c
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:39:37 +0300

    feat: add interface for organization service

---
+++ src/application/contracts/i_organization_service.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from src.application.abstractions.repositories.i_organization_repository import IOrganizationRepository
+from src.application.contracts.base import Service
+from src.application.domain.entities.organization import OrganizationEntity
+
+
+class IOrganizationService(Service, ABC):
+
+    def __init__(self, repository: IOrganizationRepository):
+        self._repository = repository
+
+    @abstractmethod
+    async def create(self, name: str) -> OrganizationEntity:
+        raise NotImplementedError
