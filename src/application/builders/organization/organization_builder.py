5eddba600f994101519fced951e6784e9c9d234e
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:41:15 +0300

    feat: impl interface organization builder

---
+++ src/application/builders/organization/organization_builder.py
@@ -0,0 +1,18 @@
+from src.application.abstractions.repositories.i_organization_repository import IOrganizationRepository
+from src.application.builders.organization.i_organization_builder import IOrganizationBuilder
+from src.application.contracts.i_organization_service import IOrganizationService
+from src.application.services.organization_service import OrganizationService
+
+class OrganizationBuilder(IOrganizationBuilder):
+
+    def __init__(self):
+        self._repository = None
+
+    def set_repository(self, repository: IOrganizationRepository) -> 'OrganizationBuilder':
+        self._repository = repository
+        return self
+
+    def build(self) -> IOrganizationService:
+        if not self._repository:
+            raise ValueError("Repository must be set before building the service.")
+        return OrganizationService(self._repository)
