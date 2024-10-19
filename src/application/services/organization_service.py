7e9835227bfecdbc7c3f27fc055d9c26669fba2e
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:40:43 +0300

    feat: impl organization service

---
+++ src/application/services/organization_service.py
@@ -0,0 +1,15 @@
+from src.application.abstractions.repositories.i_organization_repository import IOrganizationRepository
+from src.application.contracts.i_organization_service import IOrganizationService
+from src.application.domain.entities.organization import OrganizationEntity
+from src.application.domain.value_objects.name import Name
+
+
+class OrganizationService(IOrganizationService):
+
+    def __init__(self, repository: IOrganizationRepository):
+        super().__init__(repository)
+
+    async def create(self, name: str) -> OrganizationEntity:
+        organization_entity = OrganizationEntity(name=Name(name))
+        organization = await self._repository.create(organization_entity)
+        return organization
