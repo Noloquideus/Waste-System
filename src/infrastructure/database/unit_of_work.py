dc8655a962f7fd17f0fcdcf56ec61ca34790cc24
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:41:40 +0300

    feat: add organization dao

--- src/infrastructure/database/unit_of_work.py
+++ src/infrastructure/database/unit_of_work.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
+from src.application.abstractions.daos.i_organization_dao import IOrganizationDao
+from src.infrastructure.database.daos.organization_dao import OrganizationDao
 from src.infrastructure.database.database import async_session_maker
 
 
 class IUnitOfWork(ABC):
+
+    organization_dao: IOrganizationDao = None
 
     def __init__(self):
         self.session_factory = async_session_maker
@@ -28,6 +32,7 @@ class UnitOfWork(IUnitOfWork):
 
     async def __aenter__(self):
         self.__session = self.session_factory()
+        self.organization_dao = OrganizationDao(self.__session)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
