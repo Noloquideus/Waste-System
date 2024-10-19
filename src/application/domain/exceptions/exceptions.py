bbde2201db28c710680cbfefb8d2aa2b94f82a5a
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:45:31 +0300

    feat: add NotFoundException

--- src/application/domain/exceptions/exceptions.py
+++ src/application/domain/exceptions/exceptions.py
@@ -6,3 +6,7 @@ from src.core.objects.sealed import Sealed
 class ValidationException(Sealed, ApplicationException):
     def __init__(self, status_code: StatusCode = StatusCode.BAD_REQUEST.value, message: str = 'Validation error'):
         super().__init__(status_code=status_code, message=message)
+
+class NotFoundException(Sealed, ApplicationException):
+    def __init__(self, status_code: StatusCode = StatusCode.NOT_FOUND.value, message: str = 'Not found'):
+        super().__init__(status_code=status_code, message=message)
