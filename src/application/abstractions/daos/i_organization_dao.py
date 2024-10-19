ce95ff61af5900a052a5075d6f1654c077436517
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:39:51 +0300

    feat: add interface for organization dao

---
+++ src/application/abstractions/daos/i_organization_dao.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Union
+from src.application.abstractions.daos.base import Dao
+from src.application.domain.entities.organization import OrganizationEntity
+from src.infrastructure.database.models.organization import Organization
+
+
+class IOrganizationDao(Dao, ABC):
+
+    @abstractmethod
+    async def create(self, organization_entity: OrganizationEntity) -> Organization:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def _get_by_name(self, name: str) -> Union[Organization, None]:
+        raise NotImplementedError
