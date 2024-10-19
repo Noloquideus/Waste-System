e3c10b41b810ed09fccae9470ddf21666367ad9f
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 02:41:32 +0300

    feat: add container

---
+++ src/container.py
@@ -0,0 +1,41 @@
+from typing import Dict, Type
+from src.application.contracts.base import Service
+from src.infrastructure.database.unit_of_work import IUnitOfWork, UnitOfWork
+from src.application.builders.base import Builder
+from src.application.builders.organization.organization_builder import OrganizationBuilder
+from src.application.contracts.i_organization_service import IOrganizationService
+from src.application.abstractions.repositories.i_organization_repository import IOrganizationRepository
+from src.infrastructure.database.repositories.organizatrion_repository import OrganizationRepository
+
+class Container:
+
+    __unit_of_work: IUnitOfWork = UnitOfWork
+
+    # Dictionary for storing builder types and their corresponding interfaces
+    __service_builders: Dict[Type[Service], Type[Builder]] = {
+        IOrganizationService: OrganizationBuilder
+    }
+
+    # Dictionary for storing correspondence between services and repositories
+    __service_repositories: Dict[Type[Service], Type[IOrganizationRepository]] = {
+        IOrganizationService: OrganizationRepository
+    }
+
+    @staticmethod
+    async def get_service(service_type: Type[Service]) -> Service:
+
+        if service_type not in Container.__service_builders:
+            raise ValueError(f"No builder registered for service type {service_type}")
+
+        if service_type not in Container.__service_repositories:
+            raise ValueError(f"No repository registered for service type {service_type}")
+
+        builder_class = Container.__service_builders[service_type]()
+        repository_class = Container.__service_repositories[service_type]
+
+        unit_of_work = Container.__unit_of_work()
+
+        repository = repository_class(unit_of_work)
+
+        service = builder_class.set_repository(repository).build()
+        return service
