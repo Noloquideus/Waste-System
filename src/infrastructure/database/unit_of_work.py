17cc9d292a77ace1eacc48af4546100a670277b2
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 06:06:51 +0300

    feat: add storage dao

--- src/infrastructure/database/unit_of_work.py
+++ src/infrastructure/database/unit_of_work.py
@@ -1,15 +1,18 @@
 from abc import ABC, abstractmethod
 from src.application.abstractions.daos.i_organization_dao import IOrganizationDao
+from src.application.abstractions.daos.i_storage_dao import IStorageDao
 from src.application.abstractions.daos.i_waste_type_dao import IWasteTypeDao
 from src.infrastructure.database.daos.organization_dao import OrganizationDao
+from src.infrastructure.database.daos.storage_dao import StorageDao
 from src.infrastructure.database.daos.waste_type_dao import WasteTypeDao
 from src.infrastructure.database.database import async_session_maker
-
+from src.logger import logger
 
 class IUnitOfWork(ABC):
 
     organization_dao: IOrganizationDao = None
     waste_type_dao: IWasteTypeDao = None
+    storage_dao: IStorageDao = None
 
     def __init__(self):
         self.session_factory = async_session_maker
@@ -34,20 +37,34 @@ class IUnitOfWork(ABC):
 class UnitOfWork(IUnitOfWork):
 
     async def __aenter__(self):
+        logger.debug('Unit of work created')
         self.__session = self.session_factory()
         self.organization_dao = OrganizationDao(self.__session)
         self.waste_type_dao = WasteTypeDao(self.__session)
+        self.storage_dao = StorageDao(self.__session)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
+        logger.debug('Unit of work closed')
         if exc_type:
+            logger.debug(exc_tb)
+            logger.debug('Rolling back')
+            logger.error(exc_val)
             await self.rollback()
         else:
+            logger.debug('Committing')
+            await self.__session.flush()
+            logger.debug('Session flushed')
             await self.commit()
+            logger.debug('Session commited')
+
+        logger.debug('Session closed')
         await self.__session.close()
 
     async def commit(self):
+        logger.debug('Committing')
         await self.__session.commit()
 
     async def rollback(self):
+        logger.debug('Rolling back')
         await self.__session.rollback()
