1ca25b964df71e254fc5049c30f6f28537a9c01e
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 04:53:58 +0300

    feat: impl waste type dao

---
+++ src/infrastructure/database/daos/waste_type_dao.py
@@ -0,0 +1,17 @@
+from typing import List
+from sqlalchemy import select
+from src.application.abstractions.daos.i_waste_type_dao import IWasteTypeDao
+from src.infrastructure.database.models import WasteType
+from src.logger import logger
+
+class WasteTypeDao(IWasteTypeDao):
+    async def get_all(self) -> List[WasteType]:
+        logger.info('Getting all waste types')
+        query = select(WasteType)
+        logger.debug(f'Query: {query}')
+        result = await self._session.execute(query)
+        logger.debug(f'Result: {result}')
+        waste_types = result.scalars().all()
+        logger.debug(f'Waste types: {waste_types}')
+        logger.info('Waste types found')
+        return waste_types
