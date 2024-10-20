4000de015f4b26554e2beb86c01a065c6decd1cb
Noloquideus <daniilmanukian@gmail.com>
2024-10-20 06:07:08 +0300

    feat: add assembly for storage service

--- src/application/builders/director.py
+++ src/application/builders/director.py
@@ -1,7 +1,10 @@
 from src.application.builders.organization.organization_builder import OrganizationBuilder
+from src.application.builders.storage.storage_builder import StorageBuilder
 from src.application.builders.waste_type.waste_type_builder import WasteTypeBuilder
 from src.application.contracts.i_organization_service import IOrganizationService
+from src.application.contracts.i_storage_service import IStorageService
 from src.application.contracts.i_waste_type_service import IWasteTypeService
+from src.infrastructure.database.repositories.storage_repository import StorageRepository
 from src.infrastructure.database.repositories.waste_type_repository import WasteTypeRepository
 from src.infrastructure.database.unit_of_work import UnitOfWork, IUnitOfWork
 from src.infrastructure.database.repositories.organizatrion_repository import OrganizationRepository
@@ -33,5 +36,18 @@ class Director:
         # Use the builder to create the service
         waste_type_builder = WasteTypeBuilder()
         service = waste_type_builder.set_repository(repository).build()
+
+        return service
+
+    @staticmethod
+    async def construct_storage_service() -> IStorageService:
+        """Construct and return a service with all dependencies resolved."""
+
+        # Create repository
+        repository = StorageRepository(unit_of_work=Director.__unit_of_work())
+
+        # Use the builder to create the service
+        storage_builder = StorageBuilder()
+        service = storage_builder.set_repository(repository).build()
 
         return service
