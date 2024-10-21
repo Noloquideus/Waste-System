6a63c7ab43a47b7282af9182db55ef6a66eaf249
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:37:02 +0300

    feat: add waste transfer dao

--- src/infrastructure/database/unit_of_work.py
+++ src/infrastructure/database/unit_of_work.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 from src.application.abstractions.daos.i_organization_dao import IOrganizationDao
 from src.application.abstractions.daos.i_storage_dao import IStorageDao
+from src.application.abstractions.daos.i_waste_transfer_dao import IWasteTransferDao
 from src.application.abstractions.daos.i_waste_type_dao import IWasteTypeDao
 from src.infrastructure.database.daos.organization_dao import OrganizationDao
 from src.infrastructure.database.daos.storage_dao import StorageDao
+from src.infrastructure.database.daos.waste_transfer_dao import WasteTransferDao
 from src.infrastructure.database.daos.waste_type_dao import WasteTypeDao
 from src.infrastructure.database.database import async_session_maker
 from src.logger import logger
@@ -14,6 +16,7 @@ class IUnitOfWork(ABC):
     organization_dao: IOrganizationDao = None
     waste_type_dao: IWasteTypeDao = None
     storage_dao: IStorageDao = None
+    waste_transfer_dao: IWasteTransferDao = None
 
     def __init__(self):
         self.session_factory = async_session_maker
@@ -43,6 +46,7 @@ class UnitOfWork(IUnitOfWork):
         self.organization_dao = OrganizationDao(self.__session)
         self.waste_type_dao = WasteTypeDao(self.__session)
         self.storage_dao = StorageDao(self.__session)
+        self.waste_transfer_dao = WasteTransferDao(self.__session)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
