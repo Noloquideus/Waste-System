1e8a799cb0dc65a7e0b96a5779934e446b13c4ad
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:38:05 +0300

    feat: impl get_by_id method

--- src/infrastructure/database/daos/waste_type_dao.py
+++ src/infrastructure/database/daos/waste_type_dao.py
@@ -1,11 +1,13 @@
 from typing import List
 from sqlalchemy import select
 from src.application.abstractions.daos.i_waste_type_dao import IWasteTypeDao
+from src.application.domain.entities.waste_type import WasteTypeEntity
 from src.infrastructure.database.models import WasteType
 from src.logger import logger
 
 
 class WasteTypeDao(IWasteTypeDao):
+
     async def get_all(self) -> List[WasteType]:
         logger.info('Getting all waste types')
         query = select(WasteType)
@@ -16,3 +18,14 @@ class WasteTypeDao(IWasteTypeDao):
         logger.debug(f'Waste types: {waste_types}')
         logger.info('Waste types found')
         return waste_types
+
+    async def get_by_id(self, waste_type_entity: WasteTypeEntity) -> WasteType:
+        logger.info(f'Getting waste type with id: {waste_type_entity.id.value}')
+        query = select(WasteType).where(WasteType.id == waste_type_entity.id.value)
+        logger.debug(f'Query: {query}')
+        result = await self._session.execute(query)
+        logger.debug(f'Result: {result}')
+        waste_type = result.scalars().one_or_none()
+        logger.debug(f'Waste type: {waste_type}')
+        logger.info('Waste type found')
+        return waste_type
