c0d5689508b97a1ed1e3507032aca063bfc3da75
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:35:44 +0300

    feat: add waste transfer

--- src/container.py
+++ src/container.py
@@ -1,19 +1,20 @@
 from typing import Dict, Type
-
 from src.application.abstractions.repositories.base import Repository
 from src.application.builders.storage.storage_builder import StorageBuilder
+from src.application.builders.waste_transfer.waste_transfer_builder import WasteTransferBuilder
 from src.application.builders.waste_type.waste_type_builder import WasteTypeBuilder
 from src.application.contracts.base import Service
 from src.application.contracts.i_storage_service import IStorageService
+from src.application.contracts.i_waste_transfer_service import IWasteTransferService
 from src.application.contracts.i_waste_type_service import IWasteTypeService
 from src.application.domain.exceptions.exceptions import NotRegisteredException
 from src.infrastructure.database.repositories.storage_repository import StorageRepository
+from src.infrastructure.database.repositories.waste_transfer_repository import WasteTransferRepository
 from src.infrastructure.database.repositories.waste_type_repository import WasteTypeRepository
 from src.infrastructure.database.unit_of_work import IUnitOfWork, UnitOfWork
 from src.application.builders.base import Builder
 from src.application.builders.organization.organization_builder import OrganizationBuilder
 from src.application.contracts.i_organization_service import IOrganizationService
-from src.application.abstractions.repositories.i_organization_repository import IOrganizationRepository
 from src.infrastructure.database.repositories.organizatrion_repository import OrganizationRepository
 
 
@@ -25,14 +26,16 @@ class Container:
     __service_builders: Dict[Type[Service], Type[Builder]] = {
         IOrganizationService: OrganizationBuilder,
         IWasteTypeService: WasteTypeBuilder,
-        IStorageService: StorageBuilder
+        IStorageService: StorageBuilder,
+        IWasteTransferService: WasteTransferBuilder
     }
 
     # Dictionary for storing correspondence between services and repositories
     __service_repositories: Dict[Type[Service], Type[Repository]] = {
         IOrganizationService: OrganizationRepository,
         IWasteTypeService: WasteTypeRepository,
-        IStorageService: StorageRepository
+        IStorageService: StorageRepository,
+        IWasteTransferService: WasteTransferRepository
     }
 
     @staticmethod
