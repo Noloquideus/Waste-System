d53673bf7180ec0c47b47800adee743193b12176
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:32:11 +0300

    feat: include waste transfer router

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from starlette.middleware.cors import CORSMiddleware
 from src.presentation.api.organization.router import organization_router
 from src.presentation.api.storage.router import storage_router
 from src.logger import logger
+from src.presentation.api.waste_transfer.router import waste_transfer_router
 from src.presentation.api.waste_type.router import waste_type_router
 
 
@@ -21,8 +22,8 @@ app = FastAPI(title='Waste System', version='0.0.1', redoc_url=None, docs_url='/
 app_router = APIRouter(prefix='/api')
 app_router.include_router(organization_router)
 app_router.include_router(storage_router)
+app_router.include_router(waste_transfer_router)
 app_router.include_router(waste_type_router)
-app_router.include_router(storage_router)
 app.include_router(app_router)
 
 app.add_middleware(
