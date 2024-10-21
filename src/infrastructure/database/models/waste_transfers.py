2d2c6c79f5069dfb75cf46e96be111b7db376a58
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:37:47 +0300

    feat: add created_at field

--- src/infrastructure/database/models/waste_transfers.py
+++ src/infrastructure/database/models/waste_transfers.py
@@ -1,6 +1,7 @@
 import uuid
-from sqlalchemy import ForeignKey
-from sqlalchemy.dialects.postgresql import UUID, INTEGER
+from datetime import datetime
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.dialects.postgresql import UUID, INTEGER, TIMESTAMP
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from src.infrastructure.database.models.base import Base
 
@@ -13,6 +14,7 @@ class WasteTransfer(Base):
     storage_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('storage.id'), nullable=False)
     waste_type_id: Mapped[uuid.UUID] = mapped_column(UUID, ForeignKey('waste_type.id'), nullable=False)
     quantity: Mapped[int] = mapped_column(INTEGER, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     organization = relationship('Organization', back_populates='waste_transfers')
     storage = relationship('Storage', back_populates='waste_transfers')
