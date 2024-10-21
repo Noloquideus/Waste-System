b3d605c88764b60fe3ab77c98df47ea9c5d8f3ff
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 02:14:53 +0300

    feat: add redis

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, APIRouter
 from fastapi.openapi.docs import get_swagger_ui_html
 from starlette.middleware.cors import CORSMiddleware
+from src.infrastructure.cache.redis import init_redis
 from src.presentation.api.organization.router import organization_router
 from src.presentation.api.storage.router import storage_router
 from src.logger import logger
@@ -12,6 +13,9 @@ from src.presentation.api.waste_type.router import waste_type_router
 @asynccontextmanager
 async def lifespan(_):
     logger.info('Application is starting...')
+    await init_redis()
+    logger.info('Redis is ready!')
+    logger.info('Application is ready!')
     logger.unload_logs()
     yield
     logger.info('Application is shutting down...')
