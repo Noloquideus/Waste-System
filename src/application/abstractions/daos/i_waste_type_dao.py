2c8556c4241d0efa788501dfb2ababeac6a97269
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 01:38:10 +0300

    feat: add get_by_id method

--- src/application/abstractions/daos/i_waste_type_dao.py
+++ src/application/abstractions/daos/i_waste_type_dao.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List
 from src.application.abstractions.daos.base import Dao
+from src.application.domain.entities.waste_type import WasteTypeEntity
 from src.infrastructure.database.models import WasteType
 
 
@@ -8,4 +9,8 @@ class IWasteTypeDao(Dao, ABC):
 
     @abstractmethod
     async def get_all(self) -> List[WasteType]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_id(self, waste_type_entity: WasteTypeEntity) -> WasteType:
         raise NotImplementedError
