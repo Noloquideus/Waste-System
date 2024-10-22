841cff6d9836e7e6eeac5eab02dd92fd2a1552ac
Noloquideus <daniilmanukian@gmail.com>
2024-10-22 04:09:46 +0300

    feat: add views

---
+++ src/infrastructure/database/admin/__init__.py
@@ -0,0 +1,4 @@
+from src.infrastructure.database.admin.organization import OrganizationAdmin
+from src.infrastructure.database.admin.storage import StorageAdmin
+from src.infrastructure.database.admin.waste_transfers import WasteTransferAdmin
+from src.infrastructure.database.admin.waste_type import WasteTypeAdmin
